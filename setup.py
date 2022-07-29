f3001c990490f90af11627e8ba8a04f3d6e9b072
Jeff Wright <jeffrey.wright@wartsila.com>
2022-07-29 11:00:42 -0600

    ANother type slain.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="pytest-tui",
-    version="0.9.5",
+    version="0.9.6",
     author="Jeff Wright",
     author_email="jeff.washcloth@gmail.com",
     license="MIT",
