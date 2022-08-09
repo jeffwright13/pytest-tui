33ef24c45380f258ce3e10fba99a0d41a0149022
Jeff Wright <jeffrey.wright@wartsila.com>
2022-08-08 23:46:24 -0600

    Renamed a couple of buttons/scripts.

--- pytest_tui/html.py
+++ pytest_tui/html.py
@@ -74,18 +74,19 @@ def main():  # sourcery skip: low-code-quality, use-fstring-for-concatenation
     page = HtmlPage()
     page.create_meta_table()
 
+
+
     header = f"""<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> <html> <head> <meta http-equiv="Content-Type" content="text/html; charset=utf-8"> <title>Test Report</title> <style type="text/css"> .ansi2html-content {{ display: inline; white-space: pre-wrap; word-wrap: break-word; }} .body_foreground {{ color: #{page.config_parser['HTML_COLOR_THEME'].get('BODY_FOREGROUND_COLOR')}; }} .body_background {{ background-color: #{page.config_parser['HTML_COLOR_THEME'].get('BODY_BACKGROUND_COLOR')}; }} .inv_foreground {{ color: #{page.config_parser['HTML_COLOR_THEME'].get('INV_FOREGROUND_COLOR')}; }} .inv_background {{ background-color: #{page.config_parser['HTML_COLOR_THEME'].get('INV_BACKGROUND_COLOR')}; }} .ansi1 {{ font-weight: bold; }} .ansi31 {{ color: #aa0000; }} .ansi32 {{ color: #00aa00; }} .ansi33 {{ color: #aa5500; }} .collapsible {{ font-weight: normal; color: #{page.config_parser['HTML_COLOR_THEME'].get('COLLAPSIBLE_FOREGROUND_COLOR')}; background-color: #{page.config_parser['HTML_COLOR_THEME'].get('COLLAPSIBLE_BACKGROUND_COLOR')}; cursor: pointer; width: 100%; border: none; text-align: left; outline: none; font-size: 15px; }} .active, .collapsible:hover {{ foreground-color: #{page.config_parser['HTML_COLOR_THEME'].get('HOVER_FOREGROUND_COLOR')}; background-color: #{page.config_parser['HTML_COLOR_THEME'].get('HOVER_BACKGROUND_COLOR')}; /* Green */ color: white; }} .content {{ display: none; overflow: hidden; }} </style> </head> <body class="body_foreground body_background" style="font-size: normal;"> <pre class="ansi2html-content">"""
 
     metadata_button = f"""<div> <input type="button" id="meta_button" onclick="toggle_meta()" value="Metadata"/></div> {page.metadata_table}"""
     metadata_script = """<script type="text/javascript"> const content = document.getElementById("metadata"); content.style.display = "none"; function toggle_meta() { if (content.style.display === "none") { content.style.display = "block"; } else { content.style.display = "none"; } } </script>"""
 
-    button_start = """<button type="button" class="collapsible">"""
-    button_end = """</button> <div class="content"> <p>"""
-    test_end = """</p> </div>"""
+    section_or_result_button_start = """<button type="button" class="collapsible">"""
+    section_or_result_button_end = """</button> <div class="content"> <p>"""
+    section_or_result_end = """</p> </div>"""
+    section_or_result_button_script = """<script> var coll = document.getElementsByClassName("collapsible"); var i; for (i = 0; i < coll.length; i++) { coll[i].addEventListener("click", function() { this.classList.toggle("active"); var content = this.nextElementSibling; if (content.style.display === "block") { content.style.display = "none"; } else { content.style.display = "block"; } }); } </script>"""
 
-    button_script = """<script> var coll = document.getElementsByClassName("collapsible"); var i; for (i = 0; i < coll.length; i++) { coll[i].addEventListener("click", function() { this.classList.toggle("active"); var content = this.nextElementSibling; if (content.style.display === "block") { content.style.display = "none"; } else { content.style.display = "block"; } }); } </script>"""
-
-    trailer = """</pre>""" + button_script + """<hr> </body> </html>"""
+    trailer = """</pre>""" + section_or_result_button_script + """<hr> </body> </html>"""
 
     html_out = header + f"<h1>{HTMLOUTPUTFILE.stem}</h1>" + "<hr>"
     html_out += page.clean(
@@ -105,11 +106,11 @@ def main():  # sourcery skip: low-code-quality, use-fstring-for-concatenation
     html_out += "<hr> <h2>" + "OUTPUT SECTIONS" + "</h2>"
     for section in page.sections:
         # html_out += "<hr> <h2>" + section.upper() + "</h2>"
-        html_out += button_start + section + button_end
+        html_out += section_or_result_button_start + section + section_or_result_button_end
         test = page.clean(conv.convert(page.sections[section], full=False))
         if not test:
             test = "<p>No output</p>"
-        html_out += test + test_end
+        html_out += test + section_or_result_end
 
     # Test Results
     html_out += "<hr> <h2>" + "INDIVIDUAL TEST RESULTS" + "</h2>"
@@ -118,13 +119,13 @@ def main():  # sourcery skip: low-code-quality, use-fstring-for-concatenation
         nodes = eval(f"page.results.tests_{result}")
         if nodes:
             for node in nodes:
-                html_out += button_start + node + button_end
+                html_out += section_or_result_button_start + node + section_or_result_button_end
                 test = page.clean(
                     conv.convert(eval(f"page.results.tests_{result}[node]"), full=False)
                 )
                 if not test:
                     test = "<p>No output</p>"
-                html_out += test + test_end
+                html_out += test + section_or_result_end
 
     # Final trailer and file write
     html_out += trailer
