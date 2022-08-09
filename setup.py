7cecd875efd4363c77dd295b1f7f2eb22d9601d9
Jeff Wright <jeffrey.wright@wartsila.com>
2022-08-09 00:21:12 -0600

    Bump version from 1.1.0 => 1.1.1.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="pytest-tui",
-    version="1.1.0",
+    version="1.1.1",
     author="Jeff Wright",
     author_email="jeff.washcloth@gmail.com",
     license="MIT",
