cf1f33aaee9d6a2b5c8bdf6b27fa95ab1643d895
Jeff Wright <jeff.washcloth@gmail.com>
2022-08-18 01:16:46 -0600

    Colored (partially incorrectly) sections. Metadata in rudimentary form.

--- pytest_tui/html2.py
+++ pytest_tui/html2.py
@@ -30,22 +30,9 @@ TABS_SECTIONS = [
     "reruns_section",
 ]
 TABS_RESULTS = ["Failures", "Passes", "Skipped", "Xfails", "Xpasses", "Reruns"]
+TAB_METADATA = ["Metadata"]
 
 results = Results()
-
-
-class HtmlUtils:
-    html_escape_table = {
-        "&": "&amp;",
-        '"': "&quot;",
-        "'": "&apos;",
-        ">": "&gt;",
-        "<": "&lt;",
-    }
-
-    def html_escape(self, text):
-        """Produce entities within text."""
-        return "".join(self.html_escape_table.get(c, c) for c in text)
 
 
 class TabContent:
@@ -61,12 +48,6 @@ class TabContent:
 
     def get_all(self):
         return self.tabs
-
-    def get_all_keys(self):
-        return self.tabs.keys()
-
-    def get_all_values(self):
-        return self.tabs.values()
 
     def get_all_items(self):
         return self.tabs
@@ -96,7 +77,6 @@ class HtmlPage:
     def __init__(
         self,
     ):
-        self.html_utils = HtmlUtils()
         self.tab_content = TabContent().fetch_html()
 
     def create_header(self) -> str:
@@ -108,7 +88,7 @@ class HtmlPage:
     def create_tabs(self) -> str:
         tabs_links = [
             f"""<button class="tablinks" onclick="openTab(event, '{section}')" >{section}</button>"""
-            for section in TABS_SECTIONS
+            for section in TAB_METADATA + TABS_SECTIONS
         ]
         # tabs_links.append("""<button class="tablinks" onclick="openTab(event, 'Summary')" id="defaultOpen" >Summary</button>""")
         tab_links_section = """<div class="tab">""" + "".join(tabs_links) + """</div>"""
@@ -117,7 +97,8 @@ class HtmlPage:
             for section in TABS_SECTIONS
         ]
         tab_content_section = "".join(tab_section_content)
-        return tab_content_section + tab_links_section
+        tab_metadata_content = f"""<div id="{TAB_METADATA[0]}" class="tabcontent"> <h3>{TAB_METADATA[0]}</h3> <pre><p>{self.get_metadata()}</p></pre> </div>"""
+        return tab_links_section + tab_content_section + tab_metadata_content
 
     def create_tab_script(self) -> str:
         return """<script> function openTab(evt, tabName) { var i, tabcontent, tablinks; tabcontent = document.getElementsByClassName("tabcontent"); for (i = 0; i < tabcontent.length; i++) { tabcontent[i].style.display = "none"; } tablinks = document.getElementsByClassName("tablinks"); for (i = 0; i < tablinks.length; i++) { tablinks[i].className = tablinks[i].className.replace(" active", ""); } document.getElementById(tabName).style.display = "block"; evt.currentTarget.className += " active"; } </script>"""
@@ -131,41 +112,9 @@ class HtmlPage:
     def create_cdn(self) -> str:
         return """"""
 
-    def create_meta_script(self) -> str:
-        return """<script> const content = document.getElementById("metadata"); content.style.display = "none"; function toggle_meta() { if (content.style.display === "block") { content.style.display = "none"; } else { content.style.display = "block"; } } </script>"""
-
-    def create_meta_button_and_table(self) -> str:
-        # read environment from '=== test session starts ===' section
-        environment = []
-        e = strip_ansi(results.tui_sections.test_session_starts.content).split("\n")[1:]
-        try:
-            for line in e:
-                environment.append(line)
-                if "collecting ... " in line:
-                    break
-        except Exception:
-            raise RuntimeError("Could not identify environment as reported by Pytest.")
-
-        # create table of environment metadata ('environment')
-        metadata = json.loads(
-            [line for line in environment if "metadata:" in line][0]
-            .lstrip("metadata: ")
-            .replace("'", '"')
-        )
-        metadata.pop("JAVA_HOME")
-        metadata_table = json2table.convert(
-            metadata,
-            build_direction="LEFT_TO_RIGHT",
-            table_attributes={
-                "id": "metadata",
-                "border": "1",
-                "text-align": "left",
-                "style": "width:100%",
-                "border-collapse": "collapse",
-            },
-        )
-
-        return f"""<div> <input type="button" id="meta_button" onclick="toggle_meta()" value="Show / Hide"/> </div> {metadata_table}"""
+    def get_metadata(self) -> str:
+        lines = results.tui_sections.test_session_starts.content.split("\n")
+        return lines[3]
 
 
 def main():
@@ -177,10 +126,8 @@ def main():
     html_default_open = page.create_default_open()
     html_trailer = page.create_trailer()
     html_cdn = page.create_cdn()
-    html_metatable_script = page.create_meta_script()
-    html_meta_table = page.create_meta_button_and_table()
-    html_scripts = html_metatable_script + html_tab_script + html_default_open
-    html_out = html_header + html_meta_table + html_tabs + html_scripts + html_trailer
+    html_scripts = html_tab_script + html_default_open
+    html_out = html_header + html_tabs + html_scripts + html_trailer
 
     with open("html2.html", "w+") as f:
         f.write(html_out)
