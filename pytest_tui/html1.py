668b70a24171e749af737c0dde327833c5192777
Jeff Wright <jeff.washcloth@gmail.com>
2022-08-21 02:16:19 -0600

    Fixed isssue with non-referenced css file.

--- pytest_tui/html1.py
+++ pytest_tui/html1.py
@@ -46,21 +46,16 @@ class TabContent:
     def add(self, tab, content):
         self.tabs[tab] = content
 
-    def get(self, tab):
-        return self.tabs[tab]
-
-    def get_all(self):
-        return self.tabs
-
     def get_all_items(self):
         return self.tabs
 
     def fetch_raw(self):
         self.add(
             "summary_section",
-            results.tui_sections.lastline.content
-            + results.tui_sections.test_session_starts.content
-            + results.tui_sections.short_test_summary.content,
+            results.tui_sections.short_test_summary.content,
+            # results.tui_sections.lastline.content
+            # + results.tui_sections.test_session_starts.content
+            # + results.tui_sections.short_test_summary.content,
         )
         self.add("warnings_section", results.tui_sections.warnings_summary.content)
         self.add("errors_section", results.tui_sections.errors.content)
@@ -70,7 +65,6 @@ class TabContent:
         return self.get_all_items()
 
     def fetch_html(self):
-        reset = "\x1b[0m"
         return {
             key: self.converter.convert(value, full=False)
             for key, value in self.fetch_raw().items()
@@ -93,16 +87,10 @@ class HtmlPage:
         self.converter = Ansi2HTMLConverter()
 
     def create_header(self) -> str:
-        return """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> <html> <head> <meta http-equiv="Content-Type" content="text/html; charset=utf-8" name="viewport" content="width=device-width, initial-scale=1.0"> <title>Pytest-Tui Test Report</title> <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css"> <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto"> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">      <link rel="stylesheet" href="pytest_tui/styles.css">    <style> html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;} .w3-sidebar { z-index: 3; width: 250px; top: 43px; bottom: 0; height: inherit; } </style> </head> <body class="body_foreground body_background" style="font-family: 'Helvetica, Arial, sans-serif'; font-size: normal;" >"""
+        return """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> <html> <head> <meta http-equiv="Content-Type" content="text/html; charset=utf-8" name="viewport" content="width=device-width, initial-scale=1.0"> <title>Pytest-Tui Test Report</title> <link rel="stylesheet" href="/Users/jwr003/coding/pytest-tui/pytest_tui/styles.css" type="text/css"> <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css"> <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto"> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> </head> <body class="body_foreground body_background" style="font-family: 'Helvetica, Arial, sans-serif'; font-size: normal;" >"""
 
     def create_testrun_results(self) -> str:
-        return (
-            """<pre><b>"""
-            + self.converter.convert(
-                results.tui_sections.lastline.content.replace("=", ""), full=False
-            )
-            + """</b></pre>"""
-        )
+        return ("""<pre><b>""" + self.converter.convert( results.tui_sections.lastline.content.replace("=", ""), full=False ) + """</b></pre>""" )
 
     def create_trailer(self) -> str:
         return """</script> </body> </html>"""
@@ -110,9 +98,9 @@ class HtmlPage:
     def create_tabs(self) -> str:
         # Create tabs for 'About', sections, results, full-out, metadata
         tabs_links = [
-            f"""<button class="tablinks" onclick="openTab(event, '{section}')" >{section}</button>"""
-            for section in TAB_METADATA + TABS_RESULTS + TABS_SECTIONS + TAB_FULL_OUTPUT
-        ]
+            f"""<button class="tablinks" id="defaultOpen" onclick="openTab(event, '{section}')" >{section}</button>"""
+            for section in TAB_METADATA]
+        tabs_links.extend([f"""<button class="tablinks" onclick="openTab(event, '{section}')" >{section}</button>""" for section in TABS_RESULTS + TABS_SECTIONS + TAB_FULL_OUTPUT])
 
         tab_links_section = """<div class="tab">""" + "".join(tabs_links) + """</div>"""
 
@@ -174,8 +162,8 @@ class HtmlPage:
 
     def create_cdn(self) -> str:
         return ""
-        # return """<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>"""
-        # return """<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"> <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>"""
+        return """<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>"""
+        return """<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"> <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>"""
 
     def create_html(self) -> str:
         return self.create_header() + self.create_tabs() + self.create_trailer()
@@ -190,7 +178,7 @@ class HtmlPage:
             f.write(self.create_tab_script())
             f.write(self.create_tab_toggle_script())
             f.write(self.create_default_open())
-            f.write(self.create_cdn())
+            # f.write(self.create_cdn())
 
     def get_metadata(self) -> str:
         lines = results.tui_sections.test_session_starts.content.split("\n")
@@ -208,12 +196,7 @@ class HtmlPage:
             "text-align": "left",
             "tr": "nth-child(even) {background-color: #f2f2f2;}",
         }
-        return (
-            self.create_testrun_results()
-            + f"""<h6><b>Report generated on {datetime.now(timezone.utc).replace(microsecond=0 ).strftime('%Y-%m-%d %H:%M:%S')} UTC by pytest-tui version {__version__}</b></h6><br>"""
-            + """<h6><b>Meta Data / Environment:</b></h6>"""
-            + json2table.convert(m, table_attributes=table_attributes)
-        )
+        return ( self.create_testrun_results() + f"""<h6><b>Report generated on {datetime.now(timezone.utc).replace(microsecond=0 ).strftime('%Y-%m-%d %H:%M:%S')} UTC by pytest-tui version {__version__}</b></h6><br>""" + """<h6><b>Meta Data / Environment:</b></h6>""" + json2table.convert(m, table_attributes=table_attributes) )
 
     def get_terminal_output(self) -> str:
         with open(TERMINAL_OUTPUT_FILE, "r") as f:
@@ -232,7 +215,7 @@ def main():
     html_cdn = page.create_cdn()
     html_collapsible_result_script = page.create_collapsible_script()
     html_scripts = html_tab_script + html_default_open + html_collapsible_result_script
-    html_out = html_header + html_tabs + html_scripts + html_cdn + html_trailer
+    html_out = html_header + html_cdn + html_tabs + html_scripts + html_trailer
     pass
 
     with open(HTML_OUTPUT_FILE, "w+") as f:
