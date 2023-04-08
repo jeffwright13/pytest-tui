8a5d30c3f4aabf8769ffbaf88e8e63d5dc78393c
Jeffrey Wright <jeffrey.wright@wartsila.com>
2023-04-07 23:32:47 -0600

    Updated Selenium Base test for HTML report to include pretty much all content.Mayben ot pefect, but pretty comprehensive.

--- testing/sb/conftest.py
+++ testing/sb/conftest.py
@@ -19,15 +19,29 @@ def pom_vars():
         "ALL_TESTS_TAB_FIRST_TEST": "#All\ Tests > button:nth-child(1)",
         "ALL_TESTS_TAB_FIRST_TEST_RESULT": "#All\ Tests > div:nth-child(2) > pre",
         "FAILURES_TAB": "body > div > span.tab > div > span:nth-child(3) > button",
+        "FAILURES_TAB_FIRST_TEST": "#Failures > button:nth-child(1)",
+        "FAILURES_TAB_FIRST_TEST_RESULT": "#Failures > div:nth-child(2) > pre",
         "PASSES_TAB": "body > div > span.tab > div > span:nth-child(4) > button",
+        "PASSES_TAB_FIRST_TEST": "#Passes > button:nth-child(1)",
+        "PASSES_TAB_FIRST_TEST_RESULT": "#Passes > div:nth-child(2) > pre",
         "SKIPPED_TAB": "body > div > span.tab > div > span:nth-child(5) > button",
+        "SKIPPED_TAB_FIRST_TEST": "#Skipped > button:nth-child(1)",
+        "SKIPPED_TAB_FIRST_TEST_RESULT": "#Skipped > div:nth-child(2) > pre",
         "XFAILS_TAB": "body > div > span.tab > div > span:nth-child(6) > button",
+        "XFAILS_TAB_FIRST_TEST": "#Xfails > button:nth-child(1)",
+        "XFAILS_TAB_FIRST_TEST_RESULT": "#Xfails > div:nth-child(2) > pre",
         "XPASSES_TAB": "body > div > span.tab > div > span:nth-child(7) > button",
+        "XPASSES_TAB_FIRST_TEST": "#Xpasses > button:nth-child(1)",
+        "XPASSES_TAB_FIRST_TEST_RESULT": "#Xpasses > div:nth-child(2) > pre",
         "RERUNS_TAB": "body > div > span.tab > div > span:nth-child(8) > button",
+        "RERUNS_TAB_FIRST_TEST": "#Reruns > button:nth-child(1)",
+        "RERUNS_TAB_FIRST_TEST_RESULT": "#Reruns > div:nth-child(2) > pre",
         "FULL_OUTPUT_TAB": "body > div > span.tab > div > span:nth-child(11) > button",
+        "FULL_OUTPUT_TAB_EXPANDED_TEXT": "#Full\ Output > pre",
         "FOLDED_OUTPUT_TAB": (
             "body > div > span.tab > div > span:nth-child(12) > button"
         ),
+        # "FOLDED_OUTPUT_TAB_SUMMARY_SECTION": (
         "OUTPUT_SECTIONS_TAB": (
             "body > div > span.tab > div > span:nth-child(9) > button"
         ),
--- testing/sb/test_html_report.py
+++ testing/sb/test_html_report.py
@@ -77,42 +77,100 @@ def test_html_report_failures_tab(sb, pom_vars: dict[str, str]):
     sb.open(f"file:///{WORKDIR}/ptt_files/html_report.html")
     sb.assert_title(pom_vars["TITLE"])
     sb.assert_element(f"{pom_vars['FAILURES_TAB']}:contains('Failures')")
+    sb.click(f"{pom_vars['FAILURES_TAB']}")
+    sb.assert_element_not_visible(pom_vars["FAILURES_TAB_FIRST_TEST_RESULT"])
+    sb.click(f"{pom_vars['FAILURES_TAB_FIRST_TEST']}")
+    sb.assert_element_visible(pom_vars["FAILURES_TAB_FIRST_TEST_RESULT"])
+    sb.click(f"{pom_vars['FAILURES_TAB_FIRST_TEST']}")
+    sb.assert_element_not_visible(pom_vars["FAILURES_TAB_FIRST_TEST_RESULT"])
 
 
 def test_html_report_test_passes_tab(sb, pom_vars: dict[str, str]):
     sb.open(f"file:///{WORKDIR}/ptt_files/html_report.html")
     sb.assert_title(pom_vars["TITLE"])
     sb.assert_element(f"{pom_vars['PASSES_TAB']}:contains('Passes')")
+    sb.click(f"{pom_vars['PASSES_TAB']}")
+    sb.assert_element_not_visible(pom_vars["PASSES_TAB_FIRST_TEST_RESULT"])
+    sb.click(f"{pom_vars['PASSES_TAB_FIRST_TEST']}")
+    sb.assert_element_visible(pom_vars["PASSES_TAB_FIRST_TEST_RESULT"])
+    sb.click(f"{pom_vars['PASSES_TAB_FIRST_TEST']}")
+    sb.assert_element_not_visible(pom_vars["PASSES_TAB_FIRST_TEST_RESULT"])
 
 
 def test_html_report_test_skipped_tab(sb, pom_vars: dict[str, str]):
     sb.open(f"file:///{WORKDIR}/ptt_files/html_report.html")
     sb.assert_title(pom_vars["TITLE"])
     sb.assert_element(f"{pom_vars['SKIPPED_TAB']}:contains('Skipped')")
+    sb.click(f"{pom_vars['SKIPPED_TAB']}")
+    sb.assert_element_not_visible(pom_vars["SKIPPED_TAB_FIRST_TEST_RESULT"])
+    sb.click(f"{pom_vars['SKIPPED_TAB_FIRST_TEST']}")
+    sb.assert_element_visible(pom_vars["SKIPPED_TAB_FIRST_TEST_RESULT"])
+    sb.click(f"{pom_vars['SKIPPED_TAB_FIRST_TEST']}")
+    sb.assert_element_not_visible(pom_vars["SKIPPED_TAB_FIRST_TEST_RESULT"])
 
 
 def test_html_report_test_xfails_tab(sb, pom_vars: dict[str, str]):
     sb.open(f"file:///{WORKDIR}/ptt_files/html_report.html")
     sb.assert_title(pom_vars["TITLE"])
     sb.assert_element(f"{pom_vars['XFAILS_TAB']}:contains('Xfails')")
+    sb.click(f"{pom_vars['XFAILS_TAB']}")
+    sb.assert_element_not_visible(pom_vars["XFAILS_TAB_FIRST_TEST_RESULT"])
+    sb.click(f"{pom_vars['XFAILS_TAB_FIRST_TEST']}")
+    sb.assert_element_visible(pom_vars["XFAILS_TAB_FIRST_TEST_RESULT"])
+    sb.click(f"{pom_vars['XFAILS_TAB_FIRST_TEST']}")
+    sb.assert_element_not_visible(pom_vars["XFAILS_TAB_FIRST_TEST_RESULT"])
 
 
 def test_html_report_test_xpasses_tab(sb, pom_vars: dict[str, str]):
     sb.open(f"file:///{WORKDIR}/ptt_files/html_report.html")
     sb.assert_title(pom_vars["TITLE"])
     sb.assert_element(f"{pom_vars['XPASSES_TAB']}:contains('Xpasses')")
+    sb.click(f"{pom_vars['XPASSES_TAB']}")
+    sb.assert_element_not_visible(pom_vars["XPASSES_TAB_FIRST_TEST_RESULT"])
+    sb.click(f"{pom_vars['XPASSES_TAB_FIRST_TEST']}")
+    sb.assert_element_visible(pom_vars["XPASSES_TAB_FIRST_TEST_RESULT"])
+    sb.click(f"{pom_vars['XPASSES_TAB_FIRST_TEST']}")
+    sb.assert_element_not_visible(pom_vars["XPASSES_TAB_FIRST_TEST_RESULT"])
 
 
 def test_html_report_test_reruns_tab(sb, pom_vars: dict[str, str]):
     sb.open(f"file:///{WORKDIR}/ptt_files/html_report.html")
     sb.assert_title(pom_vars["TITLE"])
     sb.assert_element(f"{pom_vars['RERUNS_TAB']}:contains('Reruns')")
+    sb.click(f"{pom_vars['RERUNS_TAB']}")
+    sb.assert_element_not_visible(pom_vars["RERUNS_TAB_FIRST_TEST_RESULT"])
+    sb.click(f"{pom_vars['RERUNS_TAB_FIRST_TEST']}")
+    sb.assert_element_visible(pom_vars["RERUNS_TAB_FIRST_TEST_RESULT"])
+    sb.click(f"{pom_vars['RERUNS_TAB_FIRST_TEST']}")
+    sb.assert_element_not_visible(pom_vars["RERUNS_TAB_FIRST_TEST_RESULT"])
 
 
 def test_html_report_test_full_output_tab(sb, pom_vars: dict[str, str]):
     sb.open(f"file:///{WORKDIR}/ptt_files/html_report.html")
     sb.assert_title(pom_vars["TITLE"])
     sb.assert_element(f"{pom_vars['FULL_OUTPUT_TAB']}:contains('Full Output')")
+    sb.click(f"{pom_vars['FULL_OUTPUT_TAB']}")
+    sb.assert_element(
+        f"{pom_vars['FULL_OUTPUT_TAB_EXPANDED_TEXT']}:contains('== test session starts"
+        " ==')"
+    )
+    sb.assert_element(
+        f"{pom_vars['FULL_OUTPUT_TAB_EXPANDED_TEXT']}:contains('== ERRORS ==')"
+    )
+    sb.assert_element(
+        f"{pom_vars['FULL_OUTPUT_TAB_EXPANDED_TEXT']}:contains('== FAILURES ==')"
+    )
+    sb.assert_element(
+        f"{pom_vars['FULL_OUTPUT_TAB_EXPANDED_TEXT']}:contains('== PASSES ==')"
+    )
+    sb.assert_element(
+        f"{pom_vars['FULL_OUTPUT_TAB_EXPANDED_TEXT']}:contains('== short test summary"
+        " info ==')"
+    )
+    sb.assert_element(
+        f"{pom_vars['FULL_OUTPUT_TAB_EXPANDED_TEXT']}:contains('== rerun test summary"
+        " info ==')"
+    )
 
 
 def test_html_report_test_folded_output_tab(sb, pom_vars: dict[str, str]):
@@ -161,7 +219,6 @@ def test_html_report_test_output_sections_tab(sb, pom_vars: dict[str, str]):
     sb.assert_text(pom_vars["OUTPUT_SECTIONS_TAB_WARNINGS_SECTION_TEXT"])
 
     sb.hover(f"{pom_vars['OUTPUT_SECTIONS_TAB']}")
-    sb.hover(f"{pom_vars['OUTPUT_SECTIONS_TAB_ERRORS_SECTION']}")
     sb.click(f"{pom_vars['OUTPUT_SECTIONS_TAB_ERRORS_SECTION']}")
     sb.assert_text(pom_vars["OUTPUT_SECTIONS_TAB_ERRORS_SECTION_TEXT"])
 
@@ -173,7 +230,17 @@ def test_html_report_test_output_sections_tab(sb, pom_vars: dict[str, str]):
 
 def test_html_report_test_fold_actions_tab(sb, pom_vars: dict[str, str]):
     sb.open(f"file:///{WORKDIR}/ptt_files/html_report.html")
-    sb.click(f"{pom_vars['FOLD_ACTIONS_TAB']}")
-    sb.assert_text_visible("Folded")
-    sb.click(f"{pom_vars['FOLD_ACTIONS_TAB']}")
-    sb.assert_text_not_visible("Folded")
+    sb.click(pom_vars["FOLDED_OUTPUT_TAB"])
+    sb.scroll_into_view("Summary", by="tag name")
+    sb.assert_element_not_visible(pom_vars["FOLD_ACTIONS_TAB_FOLD_UNFOLD_ACTION"])
+    sb.assert_element_not_visible(pom_vars["FOLD_ACTIONS_TAB_SHOW_HIDE_ACTION"])
+    sb.hover(f"{pom_vars['FOLD_ACTIONS_TAB']}")
+    sb.find_element(pom_vars["FOLD_ACTIONS_TAB_FOLD_UNFOLD_ACTION"])
+    sb.assert_element_visible(pom_vars["FOLD_ACTIONS_TAB_FOLD_UNFOLD_ACTION"])
+    sb.find_element(pom_vars["FOLD_ACTIONS_TAB_SHOW_HIDE_ACTION"])
+    sb.assert_element_visible(pom_vars["FOLD_ACTIONS_TAB_SHOW_HIDE_ACTION"])
+    sb.click(pom_vars["FOLD_ACTIONS_TAB_FOLD_UNFOLD_ACTION"])
+    sb.wait_for_element_clickable("Summary", by="tag name")
+    sb.click(pom_vars["FOLD_ACTIONS_TAB_SHOW_HIDE_ACTION"])
+    sb.click(pom_vars["FOLD_ACTIONS_TAB_SHOW_HIDE_ACTION"])
+    sb.wait_for_element_not_visible("Summary", by="tag name")
